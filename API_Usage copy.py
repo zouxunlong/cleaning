9dc58aa78a34a2dc1b0af2d8d23ecfb0d87d3504
ZouXunlong <449326540@qq.com>
2022-04-02 11:15:30 +0800

    no message

--- API_Usage copy.py
+++ API_Usage copy.py
@@ -15,34 +15,33 @@ from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('all-MiniLM-L6-v2')
 model_fasttext = fasttext.load_model('./lid.176.bin')
 
-start_time=time.time()
+start_time = time.time()
 
 url = 'http://10.2.56.190:5005/translator'
 
-
 async def main():
-
     async with aiohttp.ClientSession() as session:
         with open("wikimedia.en-zh.zh", encoding='utf-8') as file_zh, open("wikimedia.en-zh.en", encoding='utf-8') as file_en:
             for (i, text_zh), (j, text_en) in zip(enumerate(file_zh), enumerate(file_en)):
-                if i in range(1,100):
+                if i in range(1, 100):
                     sentences_zh = []
                     sentences_en = []
                     for sentence in text_zh.strip().split('。'):
-                        language_type=model_fasttext.predict(sentence)
+                        language_type = model_fasttext.predict(sentence)
                         if language_type[0][0] == "__label__zh" and language_type[1][0] > 0.6:
                             sentences_zh.append(sentence)
 
                     for sentence in text_en.strip().split('.'):
-                        language_type=model_fasttext.predict(sentence)
+                        language_type = model_fasttext.predict(sentence)
                         if language_type[0][0] == "__label__en" and language_type[1][0] > 0.6:
                             sentences_en.append(sentence)
 
                     if len(sentences_zh) != 0 and len(sentences_en) != 0:
-                        references=[]
+                        references = []
                         for sentence_zh in sentences_zh:
 
-                            d = {'source': 'zh_SG', 'target': 'en_SG', 'query': sentence_zh}
+                            d = {'source': 'zh_SG', 'target': 'en_SG',
+                                 'query': sentence_zh}
 
                             # async with session.post(url,data=d) as resp:
                             #     r = await resp.json()
@@ -58,15 +57,16 @@ async def main():
                             #             if scores[i] > 0.65:
                             #                 fOut.write("{} | {}\n".format(sentence_zh.replace(
                             #                         "|", " "), sentences_en[i].replace("|", "")))
-                            
-                            async with session.post(url,data=d) as resp:
+
+                            async with session.post(url, data=d) as resp:
                                 r = await resp.json()
-                                reference=r["data"]["translations"][0]['translatedText']
+                                reference = r["data"]["translations"][0]['translatedText']
                                 references.append(reference)
 
-
-                        embeddings1 = model.encode(references, convert_to_tensor=True)
-                        embeddings2 = model.encode(sentences_en, convert_to_tensor=True)
+                        embeddings1 = model.encode(
+                            references, convert_to_tensor=True)
+                        embeddings2 = model.encode(
+                            sentences_en, convert_to_tensor=True)
 
                         cosine_scores = util.cos_sim(embeddings1, embeddings2)
 
