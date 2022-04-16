e36898e0ba42617b659e1bc8505df6a9ec8eab2e
ZouXunlong <449326540@qq.com>
2022-04-16 22:38:11 +0800

    yes

--- formatting_combine_files.py
+++ formatting_combine_files.py
@@ -6,12 +6,12 @@ start_time = time.time()
 
 
 def combine_files(file):
-    with open(file, encoding='utf8') as fIN, open('/home/xuanlong/dataclean/data_clean_and_extraction/noisy_cleaned.txt', 'a', encoding='utf8') as fOUT:
+    with open(file, encoding='utf8') as fIN, open('/home/zxl/ssd/WORK/data_clean/WORK/Batch7(CD7)_extracted_en_to_ms.txt', 'a', encoding='utf8') as fOUT:
         for i, sentence in enumerate(fIN):
             fOUT.write(sentence)
         print('finished one file')
 
-rootdir = '/home/xuanlong/dataclean/data_clean_and_extraction/noisy_cleaned'
+rootdir = '/home/zxl/ssd/WORK/data_clean/WORK/Batch7(CD7)_extracted'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
