62d61e6a148a7228f4e6805a2cc055e6c3f1fa5b
zxl <449326540@qq.com>
2022-04-16 16:30:31 +0800

    yes

--- extract_word_paragraph.py
+++ extract_word_paragraph.py
@@ -1,32 +1,36 @@
 import time
-import fasttext
+# import cld3
+import pycld2 as cld2
 from sentence_transformers import SentenceTransformer, util
 from docx import Document
 import os
 
-model_fasttext = fasttext.load_model('../model/lid.176.bin')
 model_sentence_transformers = SentenceTransformer("LaBSE")
 
 start_time=time.time()
 
+
 def extract(filepath,new_filepath):
     wordDoc = Document(filepath)
     sentences_en=[]
-    sentences_zh=[]
+    sentences_non_en=[]
+    
     for paragraph in wordDoc.paragraphs:
         text=paragraph.text.replace("\n", "")
         if text:
-            if model_fasttext.predict(text)[0][0] == "__label__en":
+            # language_type=cld3.get_language(text).language
+            language_type = cld2.detect(text)[2][0][1]
+            if language_type=="en":
                 sentences_en.append(text)
-            elif model_fasttext.predict(text)[0][0] == "__label__zh":
-                sentences_zh.append(text)
+            elif language_type=="ms":
+                sentences_non_en.append(text)
 
-    if len(sentences_zh) != 0 and len(sentences_en) != 0:
+    if len(sentences_en) != 0 and len(sentences_non_en) != 0:
         source_embeddings = model_sentence_transformers.encode(
-            sentences_zh, convert_to_tensor=True)
+            sentences_en, convert_to_tensor=True)
 
         target_embeddings = model_sentence_transformers.encode(
-            sentences_en, convert_to_tensor=True)
+            sentences_non_en, convert_to_tensor=True)
 
         cosine_scores = util.cos_sim(
             source_embeddings, target_embeddings)
@@ -35,20 +39,20 @@ def extract(filepath,new_filepath):
         with open(new_filepath, 'w', encoding='utf8') as fOut:
             for i in range(len(cosine_scores)):
                 for j in range(len(cosine_scores[0])):
-                    if cosine_scores[i][j] > 0.75:
-                        fOut.write("{} | {}\n".format(sentences_zh[i].replace(
-                            "|", " "), sentences_en[j].replace("|", "")))
+                    if cosine_scores[i][j] > 0.8:
+                        fOut.write("{} | {}\n".format(sentences_en[i].replace(
+                            "|", " "), sentences_non_en[j].replace("|", "")))
 
 
 
-path = '/home/zxl/ssd/WORK/data_clean/Batch_14/Retainers/Chinese'
-new_path=path.replace("Batch_14", "Batch_14_extracted")
+path = '../WORK/Batch7(CD7)/Retainers/Malay'
+new_path=path.replace("Batch7(CD7)", "Batch7(CD7)_extracted")
 
 # os.makedirs(new_path, exist_ok=False)
 # print("The new directory is created!")
 
 files = os.listdir(path)
 for f in files:
-	extract(os.path.join(path, f),os.path.join(new_path, f[:-5]+'_paragraph.txt'))
+	extract(os.path.join(path, f),os.path.join(new_path, f[:-5]+'.txt'))
 
 print("--- %s seconds ---" % (time.time() - start_time))
--- extract_word_table_multiline.py
+++ extract_word_table_multiline.py
@@ -1,12 +1,8 @@
-import re
 import time
-import fasttext
-from sentence_transformers import SentenceTransformer, util
+import pycld2 as cld2
 from docx import Document
 import os
 
-model_fasttext = fasttext.load_model('../model/lid.176.bin')
-model_sentence_transformers = SentenceTransformer("LaBSE")
 
 start_time = time.time()
 
@@ -18,55 +14,44 @@ def extract(filepath, new_filepath):
         for row in table.rows:
             sentences_pair = []
             sentences_en = []
-            sentences_zh = []
+            sentences_non_en = []
 
             if len(row.cells) >= 2:
                 for cell in row.cells:
-                    sentences_pair.append([text for text in cell.text.split('\n')])
+                    sentences_pair.append(
+                        [text for text in cell.text.split('\n')])
 
                 for sentence in sentences_pair[0]:
-                    if sentence:
-                        sentences_en.append(sentence)
+                    if sentence and not sentence.isspace():
+                        if sentence.strip()[:16].lower() == 'stayprepared.sg/':
+                            sentences_en[-1] = sentences_en[-1]+sentence
+                        elif sentence.strip().lower()[:5] not in ["call-", "callo", "frame", "step ", "langk", "butto", "heade", "headl", "point", "title", "footn"]:
+                            sentences_en.append(sentence)
+
                 for sentence in sentences_pair[1]:
-                    if sentence:
-                        sentences_zh.append(sentence)
+                    if sentence and not sentence.isspace():
+                        if sentence.strip()[:16].lower() == 'stayprepared.sg/':
+                            sentences_non_en[-1] = sentences_non_en[-1]+sentence
+                        elif sentence.strip().lower()[:5] not in ["call-", "callo", "frame", "step ", "langk", "butto", "heade", "headl", "point", "title", "footn"]:
+                            sentences_non_en.append(sentence)
 
-            if len(sentences_zh) != 0 and len(sentences_en) != 0:
-                if len(sentences_zh) == len(sentences_en):
-                    with open(new_filepath, 'a', encoding='utf8') as fOut:
-                        for i in range(len(sentences_zh)):
-                            if model_fasttext.predict(sentences_en[i])[0][0] != model_fasttext.predict(sentences_zh[i])[0][0]:
-                                fOut.write("{} | {}\n".format(sentences_en[i].replace(
-                                    "|", " "), sentences_zh[i].replace("|", "")))
-                else:
-                    clean_sentences_zh = [sentence_zh for sentence_zh in sentences_zh if model_fasttext.predict(
-                        sentence_zh)[0][0] in ["__label__zh", "__label__ja"]]
-
-                    if len(clean_sentences_zh) != 0:
-                        source_embeddings = model_sentence_transformers.encode(
-                            sentences_en, convert_to_tensor=True)
-                        target_embeddings = model_sentence_transformers.encode(
-                            clean_sentences_zh, convert_to_tensor=True)
-                        cosine_scores = util.cos_sim(
-                            source_embeddings, target_embeddings)
-
-                        with open(new_filepath, 'a', encoding='utf8') as fOut:
-                            for i in range(len(cosine_scores)):
-                                for j in range(len(cosine_scores[0])):
-                                    if cosine_scores[i][j] > 0.7:
-                                        fOut.write("{} | {}\n".format(sentences_en[i].replace(
-                                            "|", " "), clean_sentences_zh[j].replace("|", "")))
+            if len(sentences_en) != 0 and len(sentences_non_en) != 0:
+                assert len(sentences_en) == len(sentences_non_en)
+                with open(new_filepath, 'a', encoding='utf8') as fOut:
+                    for i in range(len(sentences_en)):
+                        if cld2.detect(sentences_en[i])[2][0][1] != cld2.detect(sentences_non_en[i])[2][0][1]:
+                            fOut.write("{} | {}\n".format(sentences_en[i].replace(
+                                "|", " "), sentences_non_en[i].replace("|", "")))
 
 
-path = '/home/zxl/ssd/WORK/data_clean/Batch_14/Temasek Foundation/Chinese'
+path = '../WORK/Batch7(CD7)/Temasek Foundation/Malay'
+new_path = path.replace("Batch7(CD7)", "Batch7(CD7)_extracted")
 
-new_path = path.replace("Batch_14", "Batch_14_extracted")
-
-# os.makedirs(new_path, exist_ok=False)
-# print("The new directory is created!")
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
 
 files = os.listdir(path)
 for f in files:
-    extract(os.path.join(path, f), os.path.join(new_path, f[:-5]+'_table.txt'))
+    extract(os.path.join(path, f), os.path.join(new_path, f[:-5]+'.txt'))
 
 print("--- %s seconds ---" % (time.time() - start_time))
