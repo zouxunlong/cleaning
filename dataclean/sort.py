953164c582e211d6ceea1e10ec72527780b9c3ef
zxl <449326540@qq.com>
2022-05-06 11:21:27 +0800

    yes

--- dataclean/sort.py
+++ dataclean/sort.py
@@ -1,14 +1,29 @@
+import os
 import time
-start_time = time.time()
-
-with open("/home/xuanlong/dataclean/data/ccaligned/CCAligned.en-ms") as file, open("/home/xuanlong/dataclean/data/ccaligned/CCAligned_sorted.en-ms", 'w', encoding='utf8') as fOUT:
-    list=[]
-    for line in file:
-        if float(line[:6]) < 1:
-            list.append(line)
-    list.sort(reverse=True)
-    for sentence in list:
-        fOUT.write(sentence)
 
 
-print("--- %s seconds ---" % (time.time() - start_time))
+def sort(file_path):
+    start_time = time.time()
+    with open(file_path) as fIN:
+        list = []
+        for line in fIN:
+            if float(line[:6]) < 1:
+                list.append(line)
+        list.sort(reverse=True)
+    with open(file_path, 'w', encoding='utf8') as fOUT:
+        for sentence in list:
+            fOUT.write(sentence)
+    list.clear()
+    print("finished " + file_path)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == '__main__':
+
+    rootdir = '/home/xuanlong/dataclean/data'
+
+    for root, dirs, files in os.walk(rootdir):
+        for file in files:
+            if os.path.splitext(file)[1] in {'.en-ta', '.en-zh', '.en-vi', '.en-ms', '.en-id'}:
+                file_path = os.path.join(root, file)
+                sort(file_path)
