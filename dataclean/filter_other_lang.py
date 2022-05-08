65a5a29055efe6fc80fe318de7ac228636167b1d
zxl <449326540@qq.com>
2022-05-08 22:42:36 +0800

    yes

--- dataclean/filter_other_lang.py
+++ dataclean/filter_other_lang.py
@@ -4,7 +4,6 @@ import string
 import time
 
 
-
 def lang_detect(text_for_lang_detect):
 
     lang_detected = set()
@@ -33,15 +32,30 @@ def lang_detect(text_for_lang_detect):
     return lang_detected
 
 
+def other_lang_detect(text_for_lang_detect):
+
+    other_lang_detected = False
+
+    text_for_lang_detect = re.sub(
+        "(?i)\w+@\S+\s?|http\S*\s?|www\.\S*\s?|[a-z\.]*\.sg\S*\s?|[0-9]+\s?", "", text_for_lang_detect)
+    text_for_lang_detect = text_for_lang_detect.translate(
+        str.maketrans('-', ' ', string.punctuation.replace('-', ''))).strip().lower()
+
+    if re.search('[\u4e00-\u9fff\u0B80-\u0BFF\u0400-\u04FF\uac00-\ud7a3\u3040-\u30ff\u31f0-\u31ff\u0A00-\u0A7FàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]', text_for_lang_detect):
+        other_lang_detected = True
+
+    return other_lang_detected
+
+
 def filter(file_path):
     start_time = time.time()
     with open(file_path) as fIN:
         list = []
         for line in fIN:
-            en_sent=line.split('|')[1].strip()
-            if len(lang_detect(en_sent)) == 0:
+            en_sent = line.split('|')[1].strip()
+            if not other_lang_detect(en_sent):
                 list.append(line)
-    with open(file_path, 'w', encoding='utf8') as fOUT:
+    with open(file_path+'.filtered', 'w', encoding='utf8') as fOUT:
         for sentence in list:
             fOUT.write(sentence)
     list.clear()
