1c6a766fb9a6cbed3aef6ca3beb0de749a90a1b5
zxl <449326540@qq.com>
2022-06-17 12:41:28 +0800

    yes

--- bitext_mining/nabilah_sentence_extract_and_align.py
+++ bitext_mining/nabilah_sentence_extract_and_align.py
@@ -1,10 +1,6 @@
 import os
-import json
 import plac
-from pathlib import Path
 from sentsplit.segment import SentSplit
-import html
-import pandas as pd
 from bi_text_miner import Bi_text_miner
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -23,10 +19,10 @@ def main():
 
     os.chdir(os.path.dirname(__file__))
 
-    with open('/home/zxl/ssd/WORK/data_clean/data/train.clean.sorted.rev.en', 'r', encoding='utf8') as f_in_en, \
-            open('/home/zxl/ssd/WORK/data_clean/data/train.clean.sorted.rev.id', 'r', encoding='utf8') as f_in_id, \
-            open('/home/zxl/ssd/WORK/data_clean/data/first50k_split.aligned.en', 'a', encoding='utf8') as f_out_en, \
-            open('/home/zxl/ssd/WORK/data_clean/data/first50k_split.aligned.id', 'a', encoding='utf8') as f_out_id:
+    with open('/home/xuanlong/dataclean/data/train.clean.sorted.rev.en', 'r', encoding='utf8') as f_in_en, \
+            open('/home/xuanlong/dataclean/data/train.clean.sorted.rev.id', 'r', encoding='utf8') as f_in_id, \
+            open('/home/xuanlong/dataclean/data/train.clean.sorted.rev.aligned.en', 'a', encoding='utf8') as f_out_en, \
+            open('/home/xuanlong/dataclean/data/train.clean.sorted.rev.aligned.id', 'a', encoding='utf8') as f_out_id:
 
         for (i, sentence_src), (j, sentence_tgt) in zip(enumerate(f_in_en), enumerate(f_in_id)):
             sentence_src = sentence_src.strip()
@@ -37,15 +33,13 @@ def main():
             text_list_dict = {'en': sentences_src, 'id': sentences_tgt}
 
             text_set_dict = bi_text_miner.list_to_set(text_list_dict)
-            en_zh_sentence_pair = bi_text_miner.sentence_matching(
+            en_id_sentence_pair = bi_text_miner.sentence_matching(
                 text_set_dict['en'], text_set_dict['id'])
 
-            for sentence_pair in en_zh_sentence_pair:
+            for sentence_pair in en_id_sentence_pair:
                 f_out_en.write(sentence_pair[0]+'\n')
                 f_out_id.write(sentence_pair[1]+'\n')
             
-            if i==50000:
-                break
 
 
 if __name__ == "__main__":
