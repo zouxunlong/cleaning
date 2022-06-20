1ea58186fd57cafcba61cc17853900c8da72f56f
zxl <449326540@qq.com>
2022-06-21 00:53:44 +0800

    yes

--- interative.py
+++ interative.py
@@ -6,10 +6,20 @@ from transquest.algo.sentence_level.monotransquest.run_model import MonoTransQue
 
 
 print("loading model...")
-# For siamese transquest framework 
-siamese_model_path = "./En2Zh_30k_S/EED"
-model = SiameseTransQuestModel(siamese_model_path)
-print("loading model:{} success".format(siamese_model_path))
+# For siamese transquest framework
+siamese_BLEU_model_path = "./En2Zh_30k_S/BLEU"
+siamese_COSSIM_model_path = "./En2Zh_30k_S/COSSIM"
+siamese_EED_model_path = "./En2Zh_30k_S/EED"
+siamese_TER_model_path = "./En2Zh_30k_S/TER"
+siamese_WMT20_model_path = "./En2Zh_30k_S/WMT20_pretrained"
+
+model_BLEU = SiameseTransQuestModel(siamese_BLEU_model_path)
+model_COSSIM = SiameseTransQuestModel(siamese_COSSIM_model_path)
+model_EED = SiameseTransQuestModel(siamese_EED_model_path)
+model_TER = SiameseTransQuestModel(siamese_TER_model_path)
+model_WMT20 = SiameseTransQuestModel(siamese_WMT20_model_path)
+
+print("loading models success")
 
 # For mono transquest framework
 
@@ -18,21 +28,32 @@ print("loading model:{} success".format(siamese_model_path))
 
 
 while True:
-  try:
-#    print("Input source sentence and machine translation, seperated by ||, e.g where are you? || 你在哪里？, input 'exit' to quit")
-    sys.stdout.flush()
-    
-    line=sys.stdin.readline()
-    if (line == "exit\n"):
+    try:
+        #    print("Input source sentence and machine translation, seperated by ||, e.g where are you? || 你在哪里？, input 'exit' to quit")
+
+        line = sys.stdin.readline()
+        if (line == "exit\n"):
+            break
+        source, mt = line.split('||')
+        inputs = [[source.strip(), mt.strip()]]
+        print(inputs)
+        predictions_BLEU = model_BLEU.predict(inputs, verbose=False)
+        predictions_COSSIM = model_COSSIM.predict(inputs, verbose=False)
+        predictions_EED = model_EED.predict(inputs, verbose=False)
+        predictions_TER = model_TER.predict(inputs, verbose=False)
+        predictions_WMT20 = model_WMT20.predict(inputs, verbose=False)
+        if (type(predictions_BLEU) == tuple):
+            print('predictions_BLEU:{}'.format(
+                predictions_BLEU[0]), flush=True)
+            print('predictions_COSSIM:{}'.format(
+                predictions_COSSIM[0]), flush=True)
+            print('predictions_EED:{}'.format(predictions_EED[0]), flush=True)
+            print('predictions_TER:{}'.format(predictions_TER[0]), flush=True)
+            print('predictions_WMT20:{}'.format(
+                predictions_WMT20[0]), flush=True)
+        else:
+            print(predictions_BLEU, flush=True)
+
+    except:
+        traceback.print_exc(file=open('output.log', 'a'))
         break
-    source, mt = line.split('||')
-    inputs = [[source.strip(), mt.strip()]]
-    predictions = model.predict(inputs, verbose=False)
-    if (type(predictions) == tuple):
-        print(predictions[0])
-    else:
-        print(predictions)
-    sys.stdout.flush()
-  except:
-    traceback.print_exc(file=open('output.log','a'))
-    break
