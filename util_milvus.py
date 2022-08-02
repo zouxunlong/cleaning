b37a0caed1d9b959c0234cba509a5f3fff7f4cff
zxl <449326540@qq.com>
2022-08-02 10:11:08 +0800

    yes

--- util_milvus.py
+++ util_milvus.py
@@ -5,11 +5,11 @@ import torch
 import os
 from pymongo import MongoClient, TEXT
 
-MONGODB_CONNECTION_STRING = 'mongodb://localhost:27016/'
+MONGODB_CONNECTION_STRING = 'mongodb://localhost:27017/'
 mongo_client = MongoClient(MONGODB_CONNECTION_STRING)
 
 db = mongo_client['mlops']
-collection = db['tarun_test']
+collection = db['wukui']
 
 torch.cuda.set_device(0)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -21,7 +21,8 @@ _INDEX_FILE_SIZE = 1024
 
 milvus = Milvus(host=_HOST, port=_PORT)
 
-model_sentence_transformers = SentenceTransformer('./model/labse_bert_model')
+# model_sentence_transformers = SentenceTransformer('./model/labse_bert_model')
+model_sentence_transformers = SentenceTransformer('all-mpnet-base-v2')
 
 
 def _create_collection(collection_name):
@@ -45,21 +46,19 @@ def _create_collection(collection_name):
 
 def retreive_data():
     results = collection.find(
-        {}, {'sentence_src': 1, 'sentence_tgt': 1, 'milvus_id': 1}).limit(1000)
+        {"_id":{"$lte":1000000000001000000}}, {'sentence_src': 1, 'sentence_tgt': 1, '_id': 1})
     sentences_src = []
     sentences_tgt = []
     milvus_ids = []
     for item in results:
         sentences_src.append(item['sentence_src'])
         sentences_tgt.append(item['sentence_tgt'])
-        milvus_ids.append(item['milvus_id'])
+        milvus_ids.append(item['_id'])
 
     return sentences_src, sentences_tgt, milvus_ids
 
 
 def _insert(collection_name, vectors, milvus_ids):
-
-    _create_collection(collection_name)
 
     status, ids = milvus.insert(collection_name=collection_name,
                                 records=vectors,
@@ -110,23 +109,30 @@ def query(collection_name, query):
 
 def main():
 
+    _create_collection('wukui')
+
     sentences_src, sentences_tgt, ids = retreive_data()
+
+    print('start embedding', flush = True)
 
     embeddings = model_sentence_transformers.encode(
         sentences_src, show_progress_bar=True, convert_to_numpy=True, normalize_embeddings=True)
 
+    print('end embedding', flush = True)
+
     assert len(embeddings) == len(
         ids), "length of embeddings and ids don't match"
 
-    _insert('tarun_test', embeddings, ids)
+    _insert('wukui', embeddings, ids)
 
 
 if __name__ == "__main__":
-    results=query('tarun_test','He looked backed and smiled at the men behind them, who, as he was already aware')
-    print(results.id_array)
-    print(results.distance_array)
-    for (i, milvus_id), (j, distance) in zip(enumerate(results.id_array[0]), enumerate(results.distance_array[0])):
-        print(milvus_id)
-        print(distance)
-        sentence=collection.find_one({"milvus_id":milvus_id})["sentence_src"]
-        print(sentence)
+    # results=query('tarun_test','He looked backed and smiled at the men behind them, who, as he was already aware')
+    # print(results.id_array)
+    # print(results.distance_array)
+    # for (i, milvus_id), (j, distance) in zip(enumerate(results.id_array[0]), enumerate(results.distance_array[0])):
+    #     print(milvus_id)
+    #     print(distance)
+    #     sentence=collection.find_one({"milvus_id":milvus_id})["sentence_src"]
+    #     print(sentence)
+    main()
