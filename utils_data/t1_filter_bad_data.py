a1831440d2267c189e09e94a8c0815801992727f
zxl <449326540@qq.com>
2022-08-05 21:49:23 +0800

    yes

--- utils_data/t1_filter_bad_data.py
+++ utils_data/t1_filter_bad_data.py
@@ -3,51 +3,59 @@ import string
 import time
 import plac
 from pathlib import Path
+from utils_data import Reg_Exp
 
 
-def load_words():
-    with open('/home/xuanlong/dataclean/cleaning/dataclean/words_alpha.txt') as word_file:
+def load_english_words():
+    with open('/home/xuanlong/service_extraction_5002/utils_dataclean/words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
     return valid_words
 
-english_words = load_words()
 
-punctuation = r"""!"\#$%&'()*+,-./:;<=>?@[]^_`{|}~，。、‘’“”：；【】·！￥★…《》？！（）—"""
-punctuation2none_dict = str.maketrans('', '', punctuation)
-pattern_punctuation = """[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~，。、‘’“”：；【】·！￥★…《》？！（）—]"""
-pattern_url = r"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
-pattern_email = r"[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}"
-pattern_html = r"""<("[^"]*"|'[^']*'|[^'">])*>"""
-pattern_special_charactors = r"[\x00-\x08\x0a-\x1f\x7f-\x9f\xa0]|[\ufffd\ufeff\u2000-\u200f\u2028-\u202f\u205f-\u206e]|[\↑√§¶†‡‖▪●•·]"
-pattern_emoji = r'[\U0001F1E0-\U0001F1FF\U0001F300-\U0001F64F\U0001F680-\U0001FAFF\U00002702-\U000027B0]'
-pattern_informal = r'[@#]|([^\w]|^)(http|https|www|com|hey|guy|kind\sof|ok|okay|oh|dude|we|you|your|my|me|haha|i)([^\w]|$)|\'(s|ve|re|t|m|ll|d|am|)\s|-\s'
-pattern_I = r'I\s|([^\w]|$)us([^\w]|$)'
+english_words = load_english_words()
 
 
 def mis_spelling_detected(en_sent):
-    non_en_words = [word for word in en_sent.split() if (
-        not re.search('[^a-z]', word)) and (word not in english_words)]
+
+    non_en_words = [word for word in en_sent.split()
+                    if
+                    (not re.search('[^a-z]', word))
+                    and
+                    (word not in english_words)]
     if non_en_words:
         return True
     return False
 
 
 def emoji_detected(text_for_detect):
-    if re.search(pattern_emoji, text_for_detect):
+    if re.search(Reg_Exp.pattern_emoji, text_for_detect):
         return True
     return False
 
 
 def informal_detected(text_for_detect):
-    if re.search(pattern_informal, text_for_detect, re.I):
+    if re.search(Reg_Exp.pattern_informal, text_for_detect, re.I):
         return True
-    if re.search(pattern_I, text_for_detect):
+    if re.search(Reg_Exp.pattern_I, text_for_detect):
         return True
     return False
 
 
-def unprintable_detected(text_for_detect):
-    if re.search('[^{}]'.format(string.printable), text_for_detect, re.I):
+def unwanted_character_detected(text_for_detect):
+    matchs = re.search(
+        r'[^a-zA-Z0-9\s\t{}{}{}{}{}{}{}{}{}{}]'.format(
+            Reg_Exp.pattern_punctuation[1:-1],
+            Reg_Exp.pattern_arabic[1:-1],
+            Reg_Exp.pattern_chinese[1:-1],
+            Reg_Exp.pattern_tamil[1:-1],
+            Reg_Exp.pattern_thai[1:-1],
+            Reg_Exp.pattern_russian[1:-1],
+            Reg_Exp.pattern_korean[1:-1],
+            Reg_Exp.pattern_japanese[1:-1],
+            Reg_Exp.pattern_vietnamese[1:-1],
+            Reg_Exp.pattern_emoji[1:-1],
+        ), text_for_detect, re.I)
+    if matchs:
         return True
     return False
 
@@ -60,12 +68,12 @@ def long_detected(text_for_detect):
 
 
 def short_detected(text_for_detect):
-    sub_strings = re.split(pattern_punctuation, text_for_detect)
+    sub_strings = re.split(Reg_Exp.pattern_punctuation, text_for_detect)
     if max([len(substring.split()) for substring in sub_strings]) < 7:
         return True
     return False
 
-    
+
 def chaotic_detected(text_for_chaotic_detect):
     alphabetic_text = re.sub('[^a-zA-Z]', '', text_for_chaotic_detect)
     if len(alphabetic_text)/len(text_for_chaotic_detect) < 0.5:
@@ -75,8 +83,8 @@ def chaotic_detected(text_for_chaotic_detect):
 
 def is_filtered(sentence_src, sentence_tgt):
 
-    if long_detected(sentence_src):
-        return True
+    # if long_detected(sentence_src):
+    #     return True
 
     # if short_detected(sentence_src):
     #     return True
@@ -84,11 +92,11 @@ def is_filtered(sentence_src, sentence_tgt):
     # if informal_detected(sentence_src):
     #     return True
 
-    # if mis_spelling_detected(sentence_src):
-    #     return True
+    if mis_spelling_detected(sentence_src):
+        return True
 
-    # if unprintable_detected(sentence_src+' '+sentence_tgt):
-    #     return True
+    if unwanted_character_detected(sentence_src+' '+sentence_tgt):
+        return True
 
     # if emoji_detected(sentence_src+' '+sentence_tgt):
     #     return True
@@ -96,25 +104,26 @@ def is_filtered(sentence_src, sentence_tgt):
     # if re.search("{}|{}|{}".format(pattern_special_charactors, pattern_html, pattern_email), sentence_src+' '+sentence_tgt, re.I):
     #     return True
 
-    # if len(re.findall(':', sentence_src, re.I)) != len(re.findall(':', sentence_tgt, re.I)):
-    #     return True
+    if len(re.findall(':', sentence_src, re.I)) != len(re.findall(':', sentence_tgt, re.I)):
+        return True
 
-    # if len(re.findall('\/', sentence_src, re.I)) != len(re.findall('\/', sentence_tgt, re.I)):
-    #     return True
+    if len(re.findall('\/', sentence_src, re.I)) != len(re.findall('\/', sentence_tgt, re.I)):
+        return True
 
-    # if len(re.findall(r'\\', sentence_src, re.I)) != len(re.findall(r'\\', sentence_tgt, re.I)):
-    #     return True
+    if len(re.findall(r'\\', sentence_src, re.I)) != len(re.findall(r'\\', sentence_tgt, re.I)):
+        return True
 
-    # if len(re.findall('@', sentence_src, re.I)) != len(re.findall('@', sentence_tgt, re.I)):
-    #     return True
+    if len(re.findall('@', sentence_src, re.I)) != len(re.findall('@', sentence_tgt, re.I)):
+        return True
 
-    # if len(re.findall('#', sentence_src, re.I)) != len(re.findall('#', sentence_tgt, re.I)):
-    #     return True
+    if len(re.findall('#', sentence_src, re.I)) != len(re.findall('#', sentence_tgt, re.I)):
+        return True
 
     # if chaotic_detected(sentence_src):
     #     return True
 
     return False
+
 
 @plac.opt('input_1', "Src Input File", type=Path)
 @plac.opt('input_2', "Tgt Input File", type=Path)
@@ -134,12 +143,12 @@ def main(input_1,
                 continue
             f_out_en.write(sentence_en)
             f_out_id.write(sentence_id)
-    print("finished ")
+    print("finished", flush=True)
     print("--- %s seconds ---" % (time.time() - start_time), flush=True)
 
 
 if __name__ == '__main__':
-    main('/home/xuanlong/dataclean/data/cleaned/clean_sorted2.en-zh.en',
-         '/home/xuanlong/dataclean/data/cleaned/clean_sorted2.en-zh.zh',
-         '/home/xuanlong/dataclean/data/cleaned/clean_sorted3.en-zh.en',
-         '/home/xuanlong/dataclean/data/cleaned/clean_sorted3.en-zh.zh')
+    main('/home/xuanlong/dataclean/cleaning/data/V4.en',
+         '/home/xuanlong/dataclean/cleaning/data/V4.th',
+         '/home/xuanlong/dataclean/cleaning/data/V4_t1.en',
+         '/home/xuanlong/dataclean/cleaning/data/V4_t1.th',)
