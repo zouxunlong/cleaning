0a569ed44fa6ea19b8e96b4bf41489e2478a72bc
zxl <449326540@qq.com>
2022-08-07 15:34:37 +0800

    yes

--- util_milvus.py
+++ util_milvus.py
@@ -1,13 +1,12 @@
 from sentence_transformers import SentenceTransformer, util
 from milvus import Milvus, IndexType, MetricType, Status
-from util_mongo import collection_wukui
+from util_mongodb import db
 import torch
 import os
 
 torch.cuda.set_device(0)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-milvus_collection_name = 'wukui'
 
 _HOST = 'localhost'
 _PORT = '19530'
@@ -16,20 +15,36 @@ _INDEX_FILE_SIZE = 2048
 
 milvus = Milvus(host=_HOST, port=_PORT)
 
-# model_sentence_transformers = SentenceTransformer('./model/labse_bert_model')
-# model_sentence_transformers = SentenceTransformer('all-mpnet-base-v2')
+model_sentence_transformers = SentenceTransformer('all-mpnet-base-v2')
 
 
-def _create_collection(collection_name):
+def milvus_query(collection_name, query, top_k):
+
+    query_vector = model_sentence_transformers.encode([query],
+                                                      show_progress_bar=False,
+                                                      convert_to_numpy=True,
+                                                      normalize_embeddings=True)
+    search_param = {"nprobe": 100}
+
+    param = {'collection_name': collection_name,
+             'query_records': query_vector,
+             'top_k': top_k,
+             'params': search_param}
+
+    status, results = milvus.search(**param)
+
+    return results
+
+
+def create_milvus_collection(collection_name):
 
     status, ok = milvus.has_collection(collection_name)
     if not ok:
-        param = {
-            'collection_name': collection_name,
-            'dimension': _DIM,
-            'index_file_size': _INDEX_FILE_SIZE,  # optional
-            'metric_type': MetricType.IP  # optional
-        }
+
+        param = {'collection_name': collection_name,
+                 'dimension': _DIM,
+                 'index_file_size': _INDEX_FILE_SIZE,
+                 'metric_type': MetricType.IP}
 
         milvus.create_collection(param)
 
@@ -47,51 +62,22 @@ def _insert(collection_name, vectors, milvus_ids):
 
     if not status.OK():
         print("Insert failed: {}".format(status), flush=True)
+    else:
+        milvus.flush([collection_name])
 
-    milvus.flush([collection_name])
-
-    print(milvus.count_entities(collection_name), flush=True)
     print(milvus.get_collection_stats(collection_name), flush=True)
     print(milvus.get_index_info(collection_name), flush=True)
 
 
-def query(collection_name, query):
+def mongo2milvus(collection_name):
 
-    query_vector = model_sentence_transformers.encode([query],
-                                                      show_progress_bar=False,
-                                                      convert_to_numpy=True,
-                                                      normalize_embeddings=True)
+    create_milvus_collection(collection_name)
 
-    search_param = {"nprobe": 1}
-
-    param = {
-        'collection_name': collection_name,
-        'query_records': query_vector,
-        'top_k': 10,
-        'params': search_param,
-    }
-
-    status, results = milvus.search(**param)
-
-    if status.OK():
-        print(results, flush=True)
-    else:
-        print("Search failed. ", status, flush=True)
-
-    return results
-
-
-def main():
-
-    _create_collection(milvus_collection_name)
-
-    results = collection_wukui.find({}, {'sentence_src': 1, '_id': 1})
+    results = db[collection_name].find({}, {'sentence_src': 1, '_id': 1})
     sentences_src = []
-    # sentences_tgt = []
     milvus_ids = []
     for item in results:
         sentences_src.append(item['sentence_src'])
-        # sentences_tgt.append(item['sentence_tgt'])
         milvus_ids.append(item['_id'])
 
         if item['_id'] % 50000 == 0:
@@ -99,13 +85,9 @@ def main():
             embeddings_src = model_sentence_transformers.encode(
                 sentences_src, show_progress_bar=False, convert_to_numpy=True, normalize_embeddings=True)
 
-            # embeddings_tgt = model_sentence_transformers.encode(
-            #     sentences_tgt, show_progress_bar=False, convert_to_numpy=True, normalize_embeddings=True)
-
             assert len(embeddings_src) == len(milvus_ids), "length of embeddings_src and ids don't match"
-            # assert len(embeddings_tgt) == len(milvus_ids), "length of embeddings_tgt and ids don't match"
 
-            _insert(milvus_collection_name, embeddings_src, milvus_ids)
+            _insert(collection_name, embeddings_src, milvus_ids)
 
             sentences_src.clear()
             # sentences_tgt.clear()
@@ -115,34 +97,34 @@ def main():
     embeddings_src = model_sentence_transformers.encode(
         sentences_src, show_progress_bar=False, convert_to_numpy=True, normalize_embeddings=True)
 
-    assert len(embeddings_src) == len(milvus_ids), "length of embeddings and ids don't match"
-    
-    _insert(milvus_collection_name, embeddings_src, milvus_ids)
+    assert len(embeddings_src) == len(
+        milvus_ids), "length of embeddings and ids don't match"
+
+    _insert(collection_name, embeddings_src, milvus_ids)
 
     sentences_src.clear()
-    # sentences_tgt.clear()
+
     milvus_ids.clear()
+
     print('all finished', flush=True)
 
 
-if __name__ == "__main__":
-    # results=query(collection_name,"I'll have to let you know later what I think.")
-    # print(results.id_array)
-    # print(results.distance_array)
-    # for (i, milvus_id), (j, distance) in zip(enumerate(results.id_array[0]), enumerate(results.distance_array[0])):
-    #     print(milvus_id)
-    #     print(distance)
-    #     sentence=collection.find_one({"_id":milvus_id})["sentence_src"]
-    #     print(sentence)
-
-    # main()
-    index_param = {'nlist': 20480}
-    status = milvus.create_index(milvus_collection_name,
+def create_milvus_index(collection_name, nlist):
+    index_param = {'nlist': nlist}
+    status = milvus.create_index(collection_name,
                                  IndexType.IVF_FLAT,
                                  index_param)
-    print('success', flush=True)
-    # print(milvus.drop_index(milvus_collection_name), flush=True)
+    print('sucess: {}'.format(status.OK()))
     print(milvus.list_collections(), flush=True)
-    print(milvus.get_collection_info(milvus_collection_name), flush=True)
-    print(milvus.get_collection_stats(milvus_collection_name), flush=True)
-    print(milvus.get_index_info(milvus_collection_name), flush=True)
+    print(milvus.get_collection_info(collection_name), flush=True)
+    print(milvus.get_collection_stats(collection_name), flush=True)
+    print(milvus.get_index_info(collection_name), flush=True)
+
+
+def create_milvus_index(collection_name):
+    print(milvus.drop_index(collection_name), flush=True)
+
+
+if __name__ == "__main__":
+    print(milvus.list_collections(), flush=True)
+
